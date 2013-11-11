cf77f40defec928950eca9d4f4bc2b0f0bffcde5
Homam <bluesnowball@gmail.com>
2013-11-11 15:20:15 +0400

    added body and number and country

--- HiggsApi/Controllers/HomeController.cs
+++ HiggsApi/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace HiggsApi.Controllers
@@ -21,9 +26,23 @@ namespace HiggsApi.Controllers
             ip = ip ?? Request.UserHostAddress;
             using (var dx = new Data.MobitransDataContext(ConnectionString))
             {
-                var seconds = dx.FN_SecondsSinceLastRecentVisit_ByIP(ip, 1000000);
+                var secondsTask = new Task<int?>(() => dx.FN_SecondsSinceLastRecentVisit_ByIP(ip, 1000000));
+                var countryTask = new Task<String>(() => IPLocaton(ip));
 
-                return Json(new { s = seconds, e = SubscriptionEnabled, ok = (seconds < SecondsMargin && SubscriptionEnabled) }, JsonRequestBehavior.AllowGet);
+                secondsTask.Start();
+                countryTask.Start();
+
+                Task.WaitAll(secondsTask, countryTask);
+
+                var seconds = secondsTask.Result;
+                var country = countryTask.Result;
+
+                var ok = (seconds < SecondsMargin && SubscriptionEnabled);
+
+                var number = !ok ? null : "2215";
+                var body = !ok ? null : "SUB 11";
+
+                return Json(new { s = seconds, e = SubscriptionEnabled, ok = ok, n = number, b = body }, JsonRequestBehavior.AllowGet);
             }
         }
 
@@ -51,7 +70,7 @@ namespace HiggsApi.Controllers
         {
             get
             {
-                return _SecondsMargin ?? (_SecondsMargin = int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings.Get("TimeMarginSeconds"))).Value;
+                return _SecondsMargin ?? (_SecondsMargin = int.Parse(WebConfigurationManager.AppSettings.Get("TimeMarginSeconds"))).Value;
             }
         }
 
@@ -61,6 +80,27 @@ namespace HiggsApi.Controllers
             filterContext.HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
         }
 
+
+        static string IPLocaton(string ip)
+        {
+            var ip2LocationAPIKey = WebConfigurationManager.AppSettings["ip2LocationAPIKey"];
+            var ip2LocationService = WebConfigurationManager.AppSettings["ip2LocationService"];
+            var request = (HttpWebRequest)WebRequest.Create(new Uri(string.Format(ip2LocationService, ip, ip2LocationAPIKey)));
+            var response = request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            if (responseStream == null)
+                return null;
+            var countryFromIp = JsonConvert.DeserializeObject<Country>(new StreamReader(responseStream).ReadToEnd());
+            if (countryFromIp == null || String.IsNullOrWhiteSpace(countryFromIp.GetCountryISOCodeResult) || countryFromIp.GetCountryISOCodeResult.Length != 2)
+                return null;
+
+            return countryFromIp.GetCountryISOCodeResult;
+        }
+
+        public class Country
+        {
+            public string GetCountryISOCodeResult { get; set; }
+        }
 
     }
 }
